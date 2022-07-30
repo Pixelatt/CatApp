3600f3468da5a39459f42be44cec0bb01a727be0
Pixelatt <chloeting77@gmail.com>
2022-07-30 09:54:44 +0800

    Initial Commit

---
+++ CatApp/CatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAppApp.swift
+//  CatApp
+//
+//  Created by T Krobot on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
